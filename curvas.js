ed5ebfbf436d7465578aab03b5d185ad4e5585af
Hector Ibarra <hibarra@imaginuity.com>
2023-04-08 20:43:15 -0500

    fro loop

--- curvas.js
+++ curvas.js
@@ -9,10 +9,12 @@ let elCanvas;
 let points;
 
 const sketch = ({ canvas }) => {
-   points = [
+  points = [
     new Point({ x: 200, y: 540 }),
-    new Point({ x: 400, y: 700, control: true }),
+    new Point({ x: 400, y: 700 }),
     new Point({ x: 880, y: 540 }),
+    new Point({ x: 600, y: 700 }),
+    new Point({ x: 640, y: 900 }),
   ];
 
   canvas.addEventListener("mousedown", onMouseDown);
@@ -25,13 +27,23 @@ const sketch = ({ canvas }) => {
 
     context.beginPath();
     context.moveTo(points[0].x, points[0].y);
-    context.quadraticCurveTo(
-      points[1].x,
-      points[1].y,
-      points[2].x,
-      points[2].y
-    );
+    for (let i =1; i < points.length; i += 2){
+
+        context.quadraticCurveTo(
+          points[i + 0].x,
+          points[i + 0].y,
+          points[i + 1].x,
+          points[i + 1].y
+        );
+    }
+    // context.quadraticCurveTo(
+    //   points[3].x,
+    //   points[3].y,
+    //   points[4].x,
+    //   points[4].y
+    // );
     context.stroke();
+
     points.forEach((point) => {
       point.draw(context);
     });
@@ -39,36 +51,33 @@ const sketch = ({ canvas }) => {
 };
 
 const onMouseDown = (e) => {
-	window.addEventListener('mousemove', onMouseMove);
-	window.addEventListener('mouseup', onMouseUp);
+  window.addEventListener("mousemove", onMouseMove);
+  window.addEventListener("mouseup", onMouseUp);
 
-	const x = (e.offsetX / elCanvas.offsetWidth)  * elCanvas.width;
-	const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
 
-	let hit = false;
-	points.forEach(point => {
-		point.isDragging = point.hitTest(x, y);
-		if (!hit && point.isDragging) hit = true;
-	});
-
-	if (!hit) points.push(new Point({ x, y }));
+  let hit = false;
+  points.forEach((point) => {
+    point.isDragging = point.hitTest(x, y);
+  });
 };
 
 const onMouseMove = (e) => {
-	const x = (e.offsetX / elCanvas.offsetWidth)  * elCanvas.width;
-	const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
 
-	points.forEach(point => {
-		if (point.isDragging) {
-			point.x = x;
-			point.y = y;
-		}
-	});
+  points.forEach((point) => {
+    if (point.isDragging) {
+      point.x = x;
+      point.y = y;
+    }
+  });
 };
 
 const onMouseUp = () => {
-	window.removeEventListener('mousemove', onMouseMove);
-	window.removeEventListener('mouseup', onMouseUp);
+  window.removeEventListener("mousemove", onMouseMove);
+  window.removeEventListener("mouseup", onMouseUp);
 };
 
 canvasSketch(sketch, settings);
