e0488c72c3566dcf61528ed68079a46c1465cee8
Hector Ibarra <hibarra@imaginuity.com>
2023-04-08 22:46:56 -0500

    multi point curves

--- curvas.js
+++ curvas.js
@@ -25,17 +25,48 @@ const sketch = ({ canvas }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
+    context.strokeStyle = "#999";
     context.beginPath();
     context.moveTo(points[0].x, points[0].y);
-    for (let i =1; i < points.length; i += 2){
 
-        context.quadraticCurveTo(
-          points[i + 0].x,
-          points[i + 0].y,
-          points[i + 1].x,
-          points[i + 1].y
-        ); 
+    for (let i =1; i < points.length; i++){
+        context.lineTo(points[i].x, points[i].y); 
     }
+    context.stroke();  
+
+    // context.beginPath();
+    // context.moveTo(points[0].x, points[0].y);
+    // for (let i =1; i < points.length; i += 2){
+
+    //     context.quadraticCurveTo(
+    //       points[i + 0].x,
+    //       points[i + 0].y,
+    //       points[i + 1].x,
+    //       points[i + 1].y
+    //     ); 
+    // }
+    // context.stroke();  
+
+    // context.beginPath();
+
+    for (let i = 0; i < points.length - 1; i++) {
+        const curr = points[i + 0];
+        const next = points[i + 1];
+
+        const mx = curr.x + (next.x - curr.x) * 0.5;
+        const my = curr.y + (next.y - curr.y) * 0.5;
+        //draw midpoints
+        // context.beginPath();
+        // context.arc(mx, my, 5, 0, Math.PI * 2);
+        // context.fillStyle = 'blue';
+        // context.fill();
+        if (i === 0) context.moveTo(curr.x, curr.y);
+        else if (i == points.length -2 ) context.quadraticCurveTo(curr.x, curr.y, next.x, next.y);
+        else context.quadraticCurveTo(curr.x, curr.y, mx, my);
+    }
+
+    context.lineWidth = 4;
+    context.strokeStyle = 'blue';
     context.stroke();
 
     points.forEach((point) => {
@@ -52,19 +83,23 @@ const onMouseDown = (e) => {
   const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
 
   let hit = false;
-  points.forEach((point) => {
+  points.forEach( point => {
     point.isDragging = point.hitTest(x, y);
+    if(!hit && point.isDragging) hit = true;
   });
+  if(!hit) points.push(new Point({x, y}));
 };
 
 const onMouseMove = (e) => {
   const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
   const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
 
+
   points.forEach((point) => {
     if (point.isDragging) {
       point.x = x;
       point.y = y;
+
     }
   });
 };
